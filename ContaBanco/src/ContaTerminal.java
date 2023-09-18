7a8c465f01134b18db43b93766827dade5989ab8
4ndr4d3 <andradesilva@mail.ru>
2023-09-18 14:44:50 -0300

    ok

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,29 +1,25 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o nome do correntista:");
-        String nome = scanner.nextLine();
+        System.out.println("Por favor, digite o número da agência:");
+        String agencia = scanner.nextLine();
 
-        System.out.println("Digite o número da conta:");
+        System.out.println("Por favor, digite o número da conta:");
         int numero = scanner.nextInt();
 
-        System.out.println("Digite o saldo:");
+        scanner.nextLine(); // Consumir o caractere de nova linha deixado no buffer de entrada
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
         double saldo = scanner.nextDouble();
 
-        System.out.println("Digite o tipo da conta (corrente ou poupança):");
-        String tipo = scanner.nextLine();
-
-        // Cria uma nova instância da classe Conta
-        Conta conta = new Conta(nome, numero, saldo, tipo);
-
-        // Imprime os dados da conta
-        System.out.println("Nome do correntista: " + conta.getNome());
-        System.out.println("Número da conta: " + conta.getNumero());
-        System.out.println("Saldo: " + conta.getSaldo());
-        System.out.println("Tipo da conta: " + conta.getTipo());
+        String mensagem = "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
     }
 }
+
